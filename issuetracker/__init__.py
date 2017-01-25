bba45f30f72dfdd50d68f7f37171f265513c2390
Matt Gathu <mattgathu@gmail.com>
2017-01-25 22:07:48 +0300

    defined flask application and its extensions
    * extensions:
        - SQLAlchemy - database ORM
        - UserManager - Flask-User's user manager

--- issuetracker/__init__.py
+++ issuetracker/__init__.py
@@ -1,4 +1,34 @@
+"""
+
+"""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_user import UserManager
 
-
+from issuetracker.utils import random_string
+# ============================================================================
+# flask app initialization
+# ============================================================================
 app = Flask(__name__)
+app.secret_key = random_string()
+
+# ============================================================================
+# load application configs from settings file
+# ============================================================================
+app.config.from_object('issuetracker.settings')
+
+# ============================================================================
+# setup application extensions
+# ============================================================================
+db = SQLAlchemy()
+db.app = app
+db.init_app(app)
+
+
+import issuetracker.views
+from issuetracker.models import *
+
+user_manager = UserManager(db_adapter, app)
+# ============================================================================
+# EOF
+# ============================================================================
