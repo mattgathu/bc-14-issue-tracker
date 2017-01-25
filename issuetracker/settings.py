24ba008cdea20067ebd1cf0c4c9c704ec6a44743
Matt Gathu <mattgathu@gmail.com>
2017-01-25 23:00:23 +0300

    flask application settings module

---
+++ issuetracker/settings.py
@@ -0,0 +1,9 @@
+"""
+
+"""
+import os
+
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///../sqlite.db")
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+USER_SEND_REGISTERED_EMAIL = False
+USER_ENABLE_LOGIN_WITHOUT_CONFIRM_EMAIL = True
