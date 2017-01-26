fdd25fa0d9ab7e3629849034dd766d7dce813c68
Matt Gathu <mattgathu@gmail.com>
2017-01-26 21:38:12 +0300

    improving api logic and code documentation

--- issuetracker/views.py
+++ issuetracker/views.py
@@ -1,11 +1,12 @@
 """
-
+File      : views.py
+Date      : January, 2017
+Author(s) : Matt Gathu <mattgathu@gmail.com>
+Desc      : flask views module
 """
 # ============================================================================
 # necessary imports
 # ============================================================================
-from email.utils import formatdate
-
 from flask import request, jsonify, abort, make_response, render_template, redirect, url_for
 from flask_user import login_required
 from flask_user import current_user
@@ -30,10 +31,12 @@ def index():
 # ============================================================================
 @app.route('/api/currentuser')
 def get_current_user():
+    """get currently logged in user """
     return jsonify({"user": current_user.serialize()})
 
 @app.route('/api/logout')
 def logout_current_user():
+    """log out current user """
     logout_user()
     return redirect(url_for('index'))
 
@@ -42,12 +45,20 @@ def logout_current_user():
 # ============================================================================
 @app.route('/api/issues', methods=['GET'])
 def get_issues():
-    issues = [issue.serialize() for issue in models.Issue.query.all()]
+    """fetch issues based on user"""
+    issues = []
+    if current_user.is_admin:
+        issues = [issue.serialize() for issue in reversed(models.Issue.query.all())]
+    else:
+        issues = [issue.serialize() for issue in
+                  reversed(models.Issue.query.filter_by(user_id=current_user.id).all())]
+
     return jsonify({"issues": issues})
 
 
 @app.route('/api/issues/<int:issue_id>', methods=['GET'])
 def get_issue(issue_id):
+    """fetch issue of id issue_id """
     issue = models.Issue.query.get(issue_id)
     if not issue:
         abort(404)
@@ -55,41 +66,55 @@ def get_issue(issue_id):
 
 @app.route('/api/issues', methods=['POST'])
 def create_issue():
+    """create a new issue """
+    # ========================================================================
     # check json data, form data and url encoded data (fuck polymer)
+    # ========================================================================
     if not request.json and not request.form and not request.args:
         abort(400)
 
+    # ========================================================================
     # aggregate into a dictionary
+    # ========================================================================
     if request.form:
-        keys = ["summary", "description", "low", "medium", "high","department", "status"]
+        keys = ["summary", "description", "low", "medium", "high", "department", "status"]
         data_dict = {key: request.form.get(key) for key in keys if key in request.form}
     elif request.json:
         data_dict = request.json
     elif request.args:
-        keys = ["summary", "description", "low", "medium", "high","department", "status"]
+        keys = ["summary", "description", "low", "medium", "high", "department", "status"]
         data_dict = {key: request.args.get(key) for key in keys if key in request.args}
-
+    # ========================================================================
     # rename keys
+    # ========================================================================
     for key in ("low", "medium", "high"):
         if key in data_dict:
             data_dict["priority"] = key
             data_dict.pop(key)
+    # ========================================================================
+    # add user id
+    # ========================================================================
+    data_dict['user_id'] = current_user.id
     # create and save new issue
     new_issue = models.Issue(**data_dict)
     new_issue.save()
 
     return jsonify({"issue": new_issue.serialize()})
 
+
 @app.route('/api/issues/<int:issue_id>', methods=['PUT'])
 def update_issue(issue_id):
+    """update issue of id issue_id """
     issue = models.Issue.query.get(issue_id)
     if not issue:
         abort(404)
     data_dict = request.json
 
+    # ========================================================================
     # if data not passed as json try getting it from url parameters
+    # ========================================================================
     if not data_dict:
-        keys = ["summary", "description", "low", "medium", "high","department"]
+        keys = ["summary", "description", "low", "medium", "high", "department", "in_progress", "status"]
         data_dict = {key: request.args.get(key) for key in keys if key in request.args}
     for key in ("low", "medium", "high"):
         if key in data_dict:
@@ -100,12 +125,16 @@ def update_issue(issue_id):
     issue.description = data_dict.get('description', issue.description)
     issue.priority = data_dict.get('priority', issue.priority)
     issue.department = data_dict.get('department', issue.department)
+    issue.in_progress = data_dict.get('in_progress', issue.in_progress)
+    issue.status = data_dict.get('status', issue.status)
     issue.save()
 
     return jsonify({'issue': issue.serialize()})
 
+
 @app.route('/api/issues/<int:issue_id>', methods=['DELETE'])
 def delete_issue(issue_id):
+    """delete issue of id issue_id """
     issue = models.Issue.query.get(issue_id)
     if not issue:
         abort(404)
@@ -120,6 +149,7 @@ def delete_issue(issue_id):
 # ============================================================================
 @app.route('/api/comments/<int:issue>', methods=['GET'])
 def get_comments(issue):
+    """fetch all comments of an issue """
     comments = [comment.serialize() for comment in
                 models.Comment.query.all() if comment.issue_id == issue]
 
@@ -128,11 +158,12 @@ def get_comments(issue):
 
 @app.route('/api/comments', methods=['POST'])
 def create_comment():
+    """create a new commentInput """
     if not request.json and not request.form and not request.args:
         abort(400)
-
+    #=========================================================================
     # aggregate into a dictionary
-    if request.form:
+    #=========================================================================    if request.form:
         keys = ["issue_id", "text"]
         data_dict = {key: request.form.get(key) for key in keys if key in request.form}
     elif request.json:
@@ -140,6 +171,11 @@ def create_comment():
     elif request.args:
         keys = ["issue_id", "text"]
         data_dict = {key: request.args.get(key) for key in keys if key in request.args}
+
+    # ========================================================================
+    # add user id
+    # ========================================================================
+    data_dict['user_id'] = current_user.id
 
     new_issue = models.Comment(**data_dict)
     new_issue.save()
@@ -152,7 +188,8 @@ def create_comment():
 # ============================================================================
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    """404 error handler """
+    return make_response(jsonify({'error': '{}'.format(error)}), 404)
 # ============================================================================
 # EOF
 # ============================================================================
