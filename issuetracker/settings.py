105317fca8a17862eb0d8cce22d5414b2a181c3b
Matt Gathu <mattgathu@gmail.com>
2017-01-26 21:37:38 +0300

    code documentation

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -1,9 +1,19 @@
 """
-
+File      : settings.py
+Date      : January, 2017
+Author(s) : Matt Gathu <mattgathu@gmail.com>
+Desc      : flask app settings module
 """
+# ============================================================================
+# necessary imports
+# ============================================================================
 import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///../sqlite.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 USER_SEND_REGISTERED_EMAIL = False
 USER_ENABLE_LOGIN_WITHOUT_CONFIRM_EMAIL = True
+TEMPLATES_AUTO_RELOAD = True
+# ============================================================================
+# EOF
+# ============================================================================
