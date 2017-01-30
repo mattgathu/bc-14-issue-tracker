8afccfc71841943524a939df962c1fda110d0758
Matt Gathu <mattgathu@gmail.com>
2017-01-30 15:57:59 +0300

    added test configs file

---
+++ issuetracker/testconfig.py
@@ -0,0 +1,15 @@
+"""
+File      : settings.py
+Date      : January, 2017
+Author(s) : Matt Gathu <mattgathu@gmail.com>
+Desc      : flask app tests settings module
+"""
+# ============================================================================
+# necessary imports
+# ============================================================================
+SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/sqlite.db"
+DEBUG = True
+TESTING = True
+# ============================================================================
+# EOF
+# ============================================================================
